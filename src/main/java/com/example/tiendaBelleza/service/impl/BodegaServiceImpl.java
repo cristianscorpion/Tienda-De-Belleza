475ccb002bae81464a914a314a4bd3905cc4acde
Cristian Camilo Roman Cardenas <cristian_33_31@hotmail.com>
2019-08-07 15:55:01 -0500

    1

--- src/main/java/com/example/tiendaBelleza/service/impl/BodegaServiceImpl.java
+++ src/main/java/com/example/tiendaBelleza/service/impl/BodegaServiceImpl.java
@@ -1,4 +1,22 @@
 package com.example.tiendaBelleza.service.impl;
 
-public class BodegaServiceImpl {
+import com.example.tiendaBelleza.converter.EntityConverter;
+import com.example.tiendaBelleza.dto.BodegaDto;
+import com.example.tiendaBelleza.repository.BodegaRepository;
+import com.example.tiendaBelleza.service.BodegaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class BodegaServiceImpl implements BodegaService {
+    @Autowired
+    BodegaRepository bodegaRepository;
+
+    @Override
+    public List<BodegaDto> getAllBodegas() {
+        return bodegaRepository.findAll().stream().map(EntityConverter::bodegaToBodegaDto).collect(Collectors.toList());
+    }
 }
--- src/main/java/com/example/tiendaBelleza/service/impl/ProductoServiceImpl.java
+++ src/main/java/com/example/tiendaBelleza/service/impl/ProductoServiceImpl.java
@@ -1,4 +1,33 @@
 package com.example.tiendaBelleza.service.impl;
 
-public class ProductoServiceImpl {
+import com.example.tiendaBelleza.converter.EntityConverter;
+import com.example.tiendaBelleza.dto.ProductoDto;
+import com.example.tiendaBelleza.repository.ProductoRepository;
+import com.example.tiendaBelleza.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+    @Autowired
+    ProductoRepository productoRepository;
+
+    @Override
+    public ProductoDto getProductByCode(Integer codigoProducto) {
+        return EntityConverter.productoToProductoDto(productoRepository.getOne(codigoProducto));
+    }
+
+    @Override
+    public void saveProduct(ProductoDto productoDto) {
+        productoRepository.save(EntityConverter.productoDtoToProducto(productoDto));
+    }
+
+    @Override
+    public List<ProductoDto> getAllProducts() {
+        return productoRepository.findAll().stream().map(EntityConverter::productoToProductoDto).collect(Collectors.toList());
+    }
 }
