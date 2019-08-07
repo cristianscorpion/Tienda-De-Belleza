7c13d92bbb74bd529c75413469ed0322354a0c41
Cristian Camilo Roman Cardenas <cristian_33_31@hotmail.com>
2019-08-07 15:56:09 -0500

    2

--- target/classes/static/js/app/app.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-var tiendaApp = angular.module('tienda', [ 'ui.bootstrap', 'tienda.controllers',
-		'tienda.services' ]);
-tiendaApp.constant("CONSTANTS", {
-	getProductByCode : "/producto/getProductByCode/",
-	getAllProducts : "/producto/getAllProducts",
-	saveProduct : "/producto/saveProduct"
-});
